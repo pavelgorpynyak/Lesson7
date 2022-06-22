c6cca2b8de4dc257e39a2133d0f295d606330425
pavel_gorpynyak <pavel_gorpynyak@bk.ru>
2022-06-22 21:45:27 +0300

    lesson7 is Finished!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        String phone = "+960-415 75-37";
+        phone = phone.replace("-", "");
+        phone = phone.replace(" ", "");
+        phone = phone.replace("+", "");
+        if (phone.length() == 10) {
+            phone = '7' + phone;
+        } else if (phone.length() > 11) {
+            throw new RuntimeException("Phone number is to long!");
+        } else if (phone.length() < 10) {
+            throw new RuntimeException("Phone number is to short!");
+        } else if (phone.length() == 11 && phone.charAt(0) != '7') {
+            throw new RuntimeException("Alarm Alarm!");
+        }
+        System.out.println("phone = " + phone);
+        String expectedPhone = "79604157537";
+        if (phone.equals(expectedPhone)) {
+            System.out.println("Success!");
+        } else {
+            System.out.println("Looser!");
+        }
     }
 }
